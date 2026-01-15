d0eac99d14e8d829bcf152256a718ef27f1d746f
Joe Petrakovich <jpetrakovich@gmail.com>
2026-01-15 10:56:57 +0900

    initial after template

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,58 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { parseYaml, Plugin, requestUrl } from 'obsidian';
+import * as z from "zod";
+import { JSONPath } from 'jsonpath-plus';
+import * as Handlebars from "handlebars";
 
-// Remember to rename these classes and interfaces!
+const Request = z.object({
+	url: z.httpUrl(),
+	method: z.enum(["GET", "POST", "PUT", "DELETE"]).default("GET"),
+	contentType: z.string().optional(),
+	body: z.string().optional(),
+	headers: z.record(z.string(), z.string()).optional(),
+	path: z.string().default("$")
+});
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class RequestPlugin extends Plugin {
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.registerMarkdownCodeBlockProcessor('request', async (source, el, ctx) => {
+			const [config, template] = source.split(/^---$/m);
+			if (!config) {
+				el.appendText("Request configuration is missing.");
+				return Promise.resolve();
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			const yaml = parseYaml(config);
+			const request = Request.safeParse(yaml);
+
+			if (!request.success) {
+				el.appendText(JSON.stringify(request.error.issues));
+				return Promise.resolve();
+			}
+
+			try {
+				const { path, ...params } = request.data;
+				const response = await requestUrl(params);
+
+				//TODO: handle response types other than json.
+
+				console.log("response: ", JSON.stringify(response));
+				const data = path ? JSONPath({ path, json: response.json }) : response.json;
+				let output = data;
+				if (template) {
+					const ht = Handlebars.compile(template);
+					output = ht(data);
 				}
-				return false;
+
+				el.append(output);
+			}
+			catch (error) {
+				console.error(error);
 			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
 	}
 
 	onunload() {
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
 }
