cb2a4cff5ab5682992bdc9fece69ca11e16a1720
Joe Petrakovich <jpetrakovich@gmail.com>
2026-01-15 23:48:26 +0900

    refactor parsing to simplify the main processor block

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+import { parseYaml } from 'obsidian';
+import * as z from "zod";
+
+const Config = z.object({
+	url: z.httpUrl(),
+	method: z.enum(["GET", "POST", "PUT", "DELETE"]).default("GET"),
+	contentType: z.string().optional(),
+	body: z.string().optional(),
+	headers: z.record(z.string(), z.string()).optional(),
+	path: z.string().optional()
+});
+export type Config = z.infer<typeof Config>;
+
+interface ParseSuccess {
+	success: true,
+	data: { config: Config, template?: string }
+}
+
+interface ParseError {
+	success: false,
+	error: z.ZodError | unknown
+	message: string
+}
+
+export type ParseResult = ParseSuccess | ParseError;
+
+export function parse(source: string): ParseResult {
+	const [yaml, template] = source.split(/^---$/m);
+
+	let result: ParseResult;
+	try {
+		const obj = parseYaml(yaml ?? '');
+		const config = Config.parse(obj);
+		result = { success: true, data: { config, template: template?.trim() } };
+	} catch (error) {
+		console.error(error);
+		result = { success: false, error, message: "Invalid configuration format." };
+		if (error instanceof z.ZodError) {
+			result.message = JSON.stringify(error.issues);
+		}
+	}
+
+	return result;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,26 @@
-import { parseYaml, Plugin, requestUrl } from 'obsidian';
+import { Plugin, requestUrl } from 'obsidian';
 import * as z from "zod";
 import { JSONPath } from 'jsonpath-plus';
 import * as Handlebars from "handlebars";
-
-const Request = z.object({
-	url: z.httpUrl(),
-	method: z.enum(["GET", "POST", "PUT", "DELETE"]).default("GET"),
-	contentType: z.string().optional(),
-	body: z.string().optional(),
-	headers: z.record(z.string(), z.string()).optional(),
-	path: z.string().optional()
-});
+import { parse } from 'config';
 
 export default class RequestPlugin extends Plugin {
 
 	async onload() {
 		this.registerMarkdownCodeBlockProcessor('request', async (source, el, ctx) => {
-			const [config, template] = source.split(/^---$/m);
-			if (!config) {
-				el.appendText("Request configuration is missing.");
-				return Promise.resolve();
-			}
+			const parsed = parse(source);
 
-			const yaml = parseYaml(config);
-			const request = Request.safeParse(yaml);
-
-			if (!request.success) {
-				el.appendText(JSON.stringify(request.error.issues));
+			if (!parsed.success) {
+				el.appendText(parsed.message);
 				return Promise.resolve();
 			}
 
 			try {
-				const { path, ...params } = request.data;
+				const { config: { path, ...params }, template } = parsed.data;
 				const response = await requestUrl(params);
-
-				//TODO: handle response types other than json.
-				console.log("response: ", JSON.stringify(response));
-
-				//TODO: consider jsonpaths output and how to use that within handlebars.
-				// JSONPath has wrap: true which wraps in array, they could target multiple.
-				// perhaps not wrapping is best, then can have default $ for path..
-				// test with multiples, common replies. may be good to have some unit tests.
 				const data = path ? JSONPath({ path, json: response.json }) : response.json;
 				let output = JSON.stringify(data);
+
 				if (template) {
 					const ht = Handlebars.compile(template);
 					output = ht(data);
@@ -52,6 +30,8 @@ export default class RequestPlugin extends Plugin {
 			}
 			catch (error) {
 				console.error(error);
+				//TODO: what are some good ways in Obsidian to pass detailed error logs?
+				el.appendText("There was an error with the request.");
 			}
 		});
 	}
